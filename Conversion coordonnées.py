f1a69c66c510075801a0e9ae4a1985702c0a9852
Yokilleurs <yokilleurs@gmail.com>
2022-09-08 20:06:19 +0200

    Update Conversion coordonnées.py

--- Conversion coordonnées.py
+++ Conversion coordonnées.py
@@ -1,8 +1,19 @@
+from math import degrees
+from numbers import Integral
+
+
 def dms_a_decimale(d, d_m, d_s):
-  DD = d_s / 3600
-  DD = DD + d_m / 60
-  DD = DD + d
-  return DD
+  decim = d_s / 3600
+  decim = decim + d_m / 60
+  decim = decim + d
+  return decim
 
 print(dms_a_decimale(12,20,42))
 
+def decimale_a_dms(decimale):
+  degres = int(decimale) # avoir l'entier (correspond au °)
+  decim = (decimale - degres) # donne à decim la valeur des décimales restantes
+  
+  return degres, decim
+
+print(decimale_a_dms(12.345))
