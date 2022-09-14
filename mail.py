84fddd2ca5dcf9ebb5051afb0c2b0f6ef5bdcc43
Yokilleurs <yokilleurs@gmail.com>
2022-09-14 23:02:52 +0200

    Update mail.py

--- mail.py
+++ mail.py
@@ -1,4 +1,10 @@
+
 def mails(nombre, t, n):
     for i in range(n):
-        nombre = nombre * (1 + t /100) + nombre # ajout de + nombre qui permet de calculer le nombre d'e-mails reçu en tout pendant n années avec un taux d'augmentation de t%
-    return nombre
+        nombre = nombre * (1 + t /100)
+        if i < 1:
+            nombre_final = nombre
+        else:
+            nombre_final = nombre_final + nombre
+    return nombre_final
+
