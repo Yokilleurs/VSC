e60c3a31abc295cc3b49e7c930524610b1b6b9f0
Yokilleurs <yokilleurs@gmail.com>
2022-09-22 09:16:35 +0200

    Rename / move

---
+++ Révision python/Conversion_bin-hexa.py
@@ -0,0 +1,9 @@
+def convhb(chaba):
+    chaba = int(chaba, 16)
+    chaba = bin(chaba)
+    return chaba
+
+def convbh(chaba):
+    chaba = int(chaba, 2)
+    chaba = hex(chaba)
+    return chaba
--- testTRUEorFALSE.py
+++
@@ -1,8 +0,0 @@
-def test(vf):
-    if vf == "true":
-        vf = 1
-    elif vf == "false":
-        vf = 0
-    else:
-        vf = "Valeur incorrecte"
-    return vf
