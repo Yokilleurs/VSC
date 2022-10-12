2ac7ad6ccb47002154c9b9e6fd911d453d38925e
Yokilleurs <yokilleurs@gmail.com>
2022-10-12 23:50:21 +0200

    eheh

--- Projets python/APIinseesiret.py
+++ Projets python/APIinseesiret.py
@@ -1,8 +1,8 @@
-#from api_insee import ApiInsee # Pour gros PC
 import time
-from APIinseesiret import ApiInsee # Pour petit PC 
+from api_insee import ApiInsee  
 
 fic = open("sirets.txt", "r")
+out = open("latency.csv", "a")
 
 api = ApiInsee(
     key = "o_kfvxFzWwDucmQrJqF7qA31Qp0a",
@@ -17,12 +17,16 @@ def appel(siret):
         print(err)
     return 1
 
+
 for i in range(100):
     sir = fic.readline() 
-    # sir = random.randint(13000545700325, 13000548109417)
-    # nombre = str(sir)
     start_time = time.time()
     appel(sir)
-    print(i, (time.time() - start_time), sir)
+    print(i, (time.time() - start_time), "secondes")
+    latence = round((time.time() - start_time), 2) 
+    out.write(str(latence))
+    out.write("\n")
+    pause(10)
 
 fic.close()
+out.close()
