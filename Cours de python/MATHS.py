c5dbf665f2e60b115c819cb58cb88d918a923116
Yokilleurs <yokilleurs@gmail.com>
2022-10-13 10:50:52 +0200

    hehe

--- Cours de python/MATHS.py
+++ Cours de python/MATHS.py
@@ -2,9 +2,18 @@ from math import *
 def delta(a, b, c):
     resultat = b**2 - 4*a*c
     if resultat == 0:
+        print("delta =", resultat)
         print("Il y a une solution")
+        x1 = (-b-sqrt(resultat))/(2*a)
+        print("x1 =", x1)
     elif resultat < 0:
+        print("delta =", resultat)
         print("Il n'y a pas de solution")
     else:
         print("Il y a deux solutions")
-    return resultat
+        x1 = (-b-sqrt(resultat))/(2*a)
+        x2 = (-b+sqrt(resultat))/(2*a)
+        print("delta =", resultat)
+        print("x1    =", x1)
+        print("x2    =", x2)
+    return  
