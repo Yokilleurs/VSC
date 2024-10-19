efb19133c114f05b47e061db862869f6ddacf1a4
Yokilleurs <yokilleurs@gmail.com>
2024-10-19 21:49:19 +0200

    Update APIinseesiret.py

--- Projets python/API insee/APIinseesiret.py
+++ Projets python/API insee/APIinseesiret.py
@@ -12,24 +12,23 @@ api = ApiInsee(
 
 def appel(siret):
     try:
-        api.siret(siret).get() #tentative d'appel à l'api
-        print('siret trouvé')
+        statut = api.siret(siret).get() # type: ignore #tentative d'appel à l'api
+        if statut == 200:
+            print('siret trouvé')
     except OSError as err: #OSError correspond à un crash des serveurs SIREN, donc on cherche à relever ces erreurs et à les gérer pour éviter un arrêt du programme...
         print(err)
-    return 1
 
+def test():
+    for i in range(100):
+        sir = fic.readline() # permet de lire dans le fichier contenant 1000 sirets
+        start_time = time.time() # début du timer pour la latence
+        appel(sir) # appel à l'api SIREN
+        print(i, (time.time() - start_time), sir) # Affichage du run + latence (arrondie à 2 décimales) + siret concerncé
+        out.write(str(round((time.time() - start_time), 4))) # ajout de la latence (arrondie à 4 décimales) au fichier de csv
+        out.write("\n") # nouvelle ligne csv
+        #time.sleep(60) # sleep 1 minute pour éviter de surcharger les serveurs de demandes (il est désactivé pour pouvoir tester le programme rapidement)...
 
-for i in range(100):
-    sir = fic.readline() # permet de lire dans le fichier contenant 1000 sirets
-    start_time = time.time() # début du timer pour la latence
-    appel(sir) # appel à l'api SIREN
-    print(i, (time.time() - start_time), sir) # Impression du run + latence + siret
-    latence = round((time.time() - start_time), 2) # calcul de la latence
-    out.write(str(latence)) # ajout de la latence au fichier de csv
-    out.write("\n") # nouvelle ligne csv
-    #time.sleep(60) # sleep 1 minute pour éviter de surcharger les serveurs de demandes (il est désactivé pour pouvoir tester le programme rapidement)...
-
-fic.close() # fermeture des fichiers ouverts au début du programme
-out.close()
+    fic.close() # fermeture des fichiers ouverts au début du programme
+    out.close()
 
 # Suite du projet : ajouter l'heure française à chaque test renvoyant une OSError pour identifier l'heure du crash des serveurs.
